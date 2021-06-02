d30980ad5127e479148cf6271614973cda1018cd
ajibade-ibrahim <ajibade.ibrahim@aurea.com>
2021-06-02 16:08:08 +0100

    Implemented BasketController

---
+++ src/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Threading.Tasks;
+using Basket.API.Entities;
+using Basket.API.Repositories.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Basket.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        public BasketController(IBasketRepository basketRepository, ILogger<BasketController> logger)
+        {
+            _basketRepository = basketRepository;
+            _logger = logger;
+        }
+
+        private readonly IBasketRepository _basketRepository;
+        private readonly ILogger<BasketController> _logger;
+
+        [HttpGet("{userName}", Name = "GetBasket")]
+        [ProducesResponseType(typeof(ShoppingCart), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string username)
+        {
+            try
+            {
+                var basket = await _basketRepository.GetBasket(username);
+                return Ok(basket ?? new ShoppingCart(username));
+            }
+            catch (Exception exception)
+            {
+                LogException(exception);
+                return GetInternalServerError();
+            }
+        }
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart cart)
+        {
+            try
+            {
+                return Ok(await _basketRepository.UpdateBasket(cart));
+            }
+            catch (Exception exception)
+            {
+                LogException(exception);
+                return GetInternalServerError();
+            }
+        }
+
+        [HttpDelete("{userName}", Name = "DeleteBasket")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> DeleteBasket(string username)
+        {
+            try
+            {
+                await _basketRepository.DeleteBasket(username);
+                return NoContent();
+            }
+            catch (Exception exception)
+            {
+                LogException(exception);
+                return GetInternalServerError();
+            }
+        }
+
+        private StatusCodeResult GetInternalServerError()
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+
+        private void LogException(Exception exception)
+        {
+            _logger.LogError($"Error occurred: {exception}");
+        }
+    }
+}
