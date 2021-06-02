91a3218f67d368420fc535b6c2b8776592cf9955
ajibade-ibrahim <ajibade.ibrahim@aurea.com>
2021-06-02 14:09:50 +0100

    Added Basket entities

---
+++ src/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart()
+        {
+        }
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        public List<ShoppingCartItem> Items { get; set; } = new();
+
+        public decimal TotalPrice
+        {
+            get { return Items.Sum(item => item.Price * item.Quantity); }
+        }
+
+        public string UserName { get; set; }
+    }
+}
