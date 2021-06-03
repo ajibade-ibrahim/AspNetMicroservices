6539e2f624ea3af6e22cdadceb510bd38ade2352
ajibade-ibrahim <ajibade.ibrahim@aurea.com>
2021-06-03 17:19:46 +0100

    Implemented Discount controller

---
+++ src/Discount/Discount.API/Controllers/DiscountController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discount.API.Entities;
+using Discount.API.Repositories.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Discount.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class DiscountController : ControllerBase
+    {
+        public DiscountController(IDiscountRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        private readonly IDiscountRepository _repository;
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Coupon), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<Coupon>> CreateDiscount([FromBody] Coupon coupon)
+        {
+            await _repository.CreateDiscount(coupon);
+            return CreatedAtRoute(
+                "GetDiscount",
+                new
+                {
+                    productName = coupon.ProductName
+                },
+                coupon);
+        }
+
+        [HttpDelete("{productName}", Name = "DeleteDiscount")]
+        [ProducesResponseType(typeof(void), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<bool>> DeleteDiscount(string productName)
+        {
+            await _repository.DeleteDiscount(productName);
+            return NoContent();
+        }
+
+        [HttpGet("{productName}", Name = "GetDiscount")]
+        [ProducesResponseType(typeof(Coupon), StatusCodes.Status200OK)]
+        public async Task<ActionResult<Coupon>> GetDiscount(string productName)
+        {
+            return Ok(await _repository.GetDiscount(productName));
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Coupon), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<Coupon>> UpdateDiscount([FromBody] Coupon coupon)
+        {
+            await _repository.UpdateDiscount(coupon);
+            return NoContent();
+        }
+    }
+}
